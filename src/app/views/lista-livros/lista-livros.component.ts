a5acfe42bb6fab0b9bd91dd77eebd11bde0b878e
soareslil <lilianectd@gmail.com>
2023-04-06 15:32:05 -0300

    operadores fix

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,48 +1,52 @@
-import { VolumeInfo } from './../../models/interfaces';
-import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Livro } from 'src/app/models/interfaces';
+import { Component, OnDestroy } from '@angular/core';
 import { LivroService } from 'src/app/service/livro.service';
+import { Livro } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css'],
+  styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
-  listaLivros: Livro[];
-  campoBusca: string = '';
-  subscription: Subscription;
-  livro: Livro;
+export class ListaLivrosComponent implements OnDestroy{
 
-  constructor(private service: LivroService) {}
+  listaLivros: Livro[];
+  campoBusca: string = ''
+  subscription: Subscription
+  livro: Livro
+
+  constructor(private service: LivroService) { }
 
   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
       next: (items) => {
         this.listaLivros = this.livrosResultadoParaLivros(items)
       },
-      error: (error) => console.error(error)
-    });
+      error: erro => console.error(erro),
+    }
+
+    )
   }
 
-  livrosResultadoParaLivros(items): Livro[]{
-    const livros:Livro[] = [];
-    items.array.forEach((item) => {
+  livrosResultadoParaLivros(items): Livro[] {
+    const livros: Livro[] = []
+
+    items.forEach(item => {
       livros.push(this.livro = {
-        title: item.VolumeInfo?.title,
-        authors: item.VolumeInfo?.authors,
-        publisher: item.VolumeInfo?.publisher,
-        publishedDate: item.VolumeInfo?.publishedDate,
-        description: item.VolumeInfo?.description,
-        previewLink: item.VolumeInfo?.previewLink,
-        thumbnail: item.VolumeInfo?.imageLinks?.thumbnail,
-      });
-    });
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+      })
+    })
+
     return livros
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.subscription.unsubscribe()
   }
 }
