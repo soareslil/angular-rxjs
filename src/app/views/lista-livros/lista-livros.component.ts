68cc409b7935d2635d2b75911ca6903c15e4dd62
soareslil <lilianectd@gmail.com>
2023-04-06 17:06:42 -0300

    otimizando busca

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,10 +1,11 @@
-import { tap, map, switchMap, filter } from 'rxjs';
+import { tap, map, switchMap, filter, debounceTime } from 'rxjs';
 import { Component } from '@angular/core';
 import { LivroService } from 'src/app/service/livro.service';
 import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { FormControl } from '@angular/forms';
 
+const PAUSA = 700;
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
@@ -16,10 +17,12 @@ export class ListaLivrosComponent {
   constructor(private service: LivroService) { }
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
     filter((valorDigitado)=> valorDigitado.length >= 3),
     tap(()=> console.log('tab---')),
     switchMap((valorDigitado)=> this.service.buscar(valorDigitado)),
-    tap(()=> console.log('requisicao-')),
+
+    tap((retornoAPI)=> console.log(retornoAPI)),
     map((items)=> this.livrosResultadoParaLivros(items))
   )
 
